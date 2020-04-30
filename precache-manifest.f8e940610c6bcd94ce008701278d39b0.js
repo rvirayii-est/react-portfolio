8ee3da7b4454fb345d3357eb77bcab536a7787d0
unknown <robertovirayii@gmail.com>
2020-05-01 03:48:31 +0800

    Updates

---
+++ precache-manifest.f8e940610c6bcd94ce008701278d39b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "94872cf7e81768d148a3",
+    "url": "/react-portfolio/static/js/runtime~main.94872cf7.js"
+  },
+  {
+    "revision": "343ef50545e985f0a616",
+    "url": "/react-portfolio/static/js/main.343ef505.chunk.js"
+  },
+  {
+    "revision": "206892ec38c99bdd1291",
+    "url": "/react-portfolio/static/js/2.206892ec.chunk.js"
+  },
+  {
+    "revision": "343ef50545e985f0a616",
+    "url": "/react-portfolio/static/css/main.87d71003.chunk.css"
+  },
+  {
+    "revision": "6d7a1aa4debd99d52b14b8c5c34bb775",
+    "url": "/react-portfolio/index.html"
+  }
+];
